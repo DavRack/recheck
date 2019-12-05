949df90255ce7987e4be720a20e1c06f4c118bbf
david <davlondonomon@unal.edu.co>
2019-12-05 13:51:02 -0500

    commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const test = [
+    {"text":"texto 1"},
+    {"text":"texto 2"},
+    {"text":"texto 3"},
+    {"text":"texto 4"},
+    {"text":"texto 5"},
+    {"text":"texto 6"},
+    {"text":"texto 7"}];
+const cont = document.getElementsByClassName("contenedor")[0];
+
+for( let i = 0; i < test.length; i++){
+    var html = ""
+    html = `
+            <div draggable="true" class="lista">
+                <div class="item">
+                    ${test[i].text}
+                </div>
+            </div>`;
+    cont.innerHTML += html;
+}
+items = document.getElementsByClassName("lista");
+new Sortable(cont, {
+    animation: 150,
+    onStart: function(evt){
+        console.log(evt.item.getElementsByClassName("item")[0].innerHTML)
+    },
+});
